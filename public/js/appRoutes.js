3adc11aa01ae2623ea2f34bb71fe00c174e212aa
Tuan TQ <tuantq.vnu@gmail.com>
2015-03-15 21:34:50 +0700

    add database schema

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -8,27 +8,27 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
 			controller: 'HomeController'
 		})
 
-		.when('/uni/:id/thongtin', {
+		.when('/uni/thongtin', {
 			templateUrl: 'views/uni_thongtin.html',
 			controller: 'UniController'
 		})
 
-        .when('/uni/:id/khoadaotao', {
+        .when('/uni/khoadaotao', {
 			templateUrl: 'views/uni_khoadaotao.html',
 			controller: 'UniController'	
 		})    
     
-		.when('/uni/:id/xemdiemchuan', {
+		.when('/uni/xemdiemchuan', {
 			templateUrl: 'views/uni_xemdiemchuan.html',
 			controller: 'UniController'	
 		})
     
-        .when('/uni/:id/tuyensinh', {
+        .when('/uni/tuyensinh', {
 			templateUrl: 'views/uni_tuyensinh.html',
 			controller: 'UniController'	
 		})
         
-        .when('/uni/:id/lienhe', {
+        .when('/uni/lienhe', {
 			templateUrl: 'views/uni_lienhe.html',
 			controller: 'UniController'	
 		})
@@ -41,7 +41,8 @@ angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', f
         .when('/dbpanel', {
             templateUrl: 'views/db_panel.html',
             controller: 'DBController'
-    })
+        })
+    
         .otherwise({
             redirecTo: '/'
         });
