13d07aaab9a688fa1e9ad525dab982a333009e8d
Tuan TQ <tuantq.vnu@gmail.com>
2015-04-16 22:56:35 +0700

    update UniController

--- public/js/controllers/UniCtrl.js
+++ public/js/controllers/UniCtrl.js
@@ -1,16 +1,17 @@
 angular.module('UniCtrl', ['UniService'])
 .controller('UniController', function($rootScope, $scope, $routeParams, getUniInforService) {
-	console.log($routeParams.id);
-	$scope.uniId = String($routeParams.id);
-	$rootScope.uniInfor = getUniInforService.query({id: $scope.uniId}, function() {
-		$scope.uniInfor = $scope.uniInfor[0];
-		console.log($scope.uniInfor);
-		for (var i = 0; i < $scope.uniInfor.majors.length; i++) {
-			var division = "";
-			for (var j = 0; j < $scope.uniInfor.majors[i].divisions.length; j++) {
-				division += String($scope.uniInfor.majors[i].divisions[j] + ", ");
+	if (typeof $rootScope.uniInfor === 'undefined') {
+		$rootScope.uniId = String($routeParams.id);
+		$rootScope.uniInfor = getUniInforService.query({id: $rootScope.uniId}, function() {
+			$rootScope.uniInfor = $rootScope.uniInfor[0];
+			console.log($rootScope.uniInfor);
+			for (var i = 0; i < $rootScope.uniInfor.majors.length; i++) {
+				var division = "";
+				for (var j = 0; j < $rootScope.uniInfor.majors[i].divisions.length; j++) {
+					division += String($rootScope.uniInfor.majors[i].divisions[j] + ", ");
+				}
+				$rootScope.uniInfor.majors[i].divisions = division.substring(0, division.length - 2);
 			}
-			$scope.uniInfor.majors[i].divisions = division.substring(0, division.length - 2);
-		}
-	});
+		});
+	}
 });
