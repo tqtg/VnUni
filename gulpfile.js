936a9940a09b4f29349b1c0abaf7c4c08a686ca6
Tuan TQ <tuantq.vnu@gmail.com>
2015-04-17 11:13:23 +0700

    gulp to minimize js file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
-var gulp = require('gulp'),
+var del = require('del'),
+    gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
     nodemon = require('gulp-nodemon'),
     livereload = require('gulp-livereload'),
-    _paths = ['app/*.js', 'public/js/*.js','public/js/controllers/*.js'];
+    _paths = ['app/*.js', 'public/js/*.js', 'public/js/controllers/*.js', 'public/js/services/*.js'];
 
 //register nodemon task
 gulp.task('nodemon', function () {
@@ -20,5 +24,18 @@ gulp.task('watch', function () {
     gulp.watch(_paths, livereload.changed);
 });
 
+gulp.task('clean', function (cb) {
+  return del(['public/libs/build'], cb);
+});
+
+ // Concatenate JS Files
+gulp.task('scripts', function() {
+    return gulp.src(['public/js/*.js', 'public/js/controllers/*.js', 'public/js/services/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify({mangle: false}))
+        .pipe(gulp.dest('public/libs/build'));
+});
+
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['nodemon', 'watch']);
+gulp.task('default', ['clean', 'scripts', 'nodemon', 'watch']);
